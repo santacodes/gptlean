3f165c6c40477442b54801d5b8bbb1766b9d5093
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2023-12-15 14:02:10 +0530

    first module commit

---
+++ modules/pspace.lean
@@ -0,0 +1,20 @@
+import Init.Data.List
+
+namespace complexity
+
+structure TuringMachine where
+  states : Type                 -- Set of states
+  symbols : Type                -- Set of symbols
+  blank : symbols                -- Blank symbol not in input alphabet
+  transition : states → symbols → states → symbols → List (ℝ × ℝ)  -- Probabilistic transition function
+
+structure TMConfiguration (TM : Type TuringMachine) where
+  state  : TM.states
+  tape  list : TM.symbols
+
+def PSPACE (TM  TuringMachine) (f : TMConfiguration TM → bool) :=
+  ∃ (poly  ℕ :ℕ), ∀ (n  ℕ), ∃ (config : TMConfiguration TM),
+    config.tape.length ≤ poly n ∧
+    (∀ (eps  ℝ), eps  0 → ∃ (k  ℕ), Pr[f TM config k] ≥ 1 - eps)
+
+end complexity
---
+++ modules/test.lean
@@ -0,0 +1,2 @@
+import Mathlib.Topology.Basic
+import Init.Data.List
