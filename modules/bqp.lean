07a0430e4df68cf05e1037c2c7fe620fbb0350ce
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2023-12-15 14:38:46 +0530

    added bqp code

---
+++ modules/bqp.lean
@@ -0,0 +1,30 @@
+-- Import necessary libraries
+import Mathlib.Init.Set
+
+-- Define the set of strings and the collection of quantum circuits
+variables {Σ : Type*} (S : set Σ) (Q : Σ → Type*)
+
+-- Define polynomial-time decidable quantum circuits
+def polynomial_time_decidable (Qx : Σ → Type*) : Type* :=
+  ∀ (x : Σ), polynomial_time_decidable (Qx x)
+
+-- Define polynomial-time generated collection of quantum circuits
+def poly_time_generated (encoding : Π (x : Σ), polynomial_time_decidable (Q x)) : Prop :=
+  ∃ (TM : polynomial_time_turing_machine),
+    ∀ (x : Σ), x ∈ S → TM.eval x = some (encoding x)
+
+-- Define a polynomial-time Turing machine
+structure polynomial_time_turing_machine :=
+  (TM : Σ → option Σ) -- Replace with the actual definition of the Turing machine
+
+-- Define the BQP class
+def BQP (S : set Σ) (Q : Σ → Type*) : Prop :=
+  poly_time_generated S Q (λ _, bool)  -- Assume the encoding is a boolean function for simplicity
+
+-- Example usage:
+variables (S : set Σ) (Q : Σ → Type*)
+
+-- Theorem statement connecting BQP and polynomial-time generation
+theorem BQP_and_poly_time_generated :
+  BQP S Q ↔ poly_time_generated S Q (λ _, bool) :=
+iff.rfl
