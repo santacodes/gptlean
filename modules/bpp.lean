0ca3c093e7c8e719bf27df480cbb54c9e208299d
san <120664435+krish-san@users.noreply.github.com>
2023-12-15 18:26:16 +0530

    Create bpp.lean

---
+++ modules/bpp.lean
@@ -0,0 +1,45 @@
+import measure_theory.probability_space
+import measure_theory.integration
+
+namespace bpp
+
+-- Definition of a probabilistic algorithm
+structure probabilistic_algorithm :=
+  (algorithm : ℕ → bool)
+  (bounded_error : ∃ (poly : ℕ → ℕ), ∀ (n : ℕ), measure_theory.prob {k | algorithm k = algorithm n} ≥ 1/2 + 1/poly n)
+
+-- BPP complexity class
+def BPP (P : probabilistic_algorithm → Prop) :=
+  ∃ (poly : ℕ → ℕ), ∀ (n : ℕ), ∀ (pa : probabilistic_algorithm),
+    P pa → (pa.algorithm n = tt → measure_theory.prob {k | pa.algorithm k = tt} ≥ 2/3) ∧ (pa.algorithm n = ff → measure_theory.prob {k | pa.algorithm k = ff} ≤ 1/3)
+
+-- Theorem: BPP is a subclass of P
+theorem BPP_is_P (P : probabilistic_algorithm → Prop) :
+  BPP P → P (λ pa, ∃ (n : ℕ), pa.algorithm n = tt) :=
+begin
+  -- Introduce the BPP assumption
+  rintros ⟨poly, h⟩,
+  
+  -- Use a specific polynomial for simplicity; replace with a suitable one
+  use (λ n, n^2 + n + 1),
+  
+  -- Introduce n and a probabilistic algorithm pa
+  intros n pa hP,
+  
+  -- Use classical logic to handle cases where the algorithm outputs true or false
+  cases' classical.em (pa.algorithm n = tt) with h1 h2,
+  
+  -- Case: pa.algorithm n = tt
+  { 
+    -- Apply the correctness property for true output
+    exact (h n pa hP).left h1,
+  },
+  
+  -- Case: pa.algorithm n = ff
+  {
+    -- Apply the error bound property for false output
+    exact (h n pa hP).right h2,
+  }
+end
+
+end bpp
