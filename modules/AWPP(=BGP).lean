87ab8d6db4adb1f9e420dbb37ff7b531d6e126eb
san <120664435+krish-san@users.noreply.github.com>
2023-12-16 18:13:20 +0530

    Create AWPP(=BGP).lean

---
+++ modules/AWPP(=BGP).lean
@@ -0,0 +1,47 @@
+import data.nat.basic
+import data.real.basic
+
+namespace awpp
+
+-- Definition of a quantum state
+structure quantum_state :=
+  (state : ℕ → ℂ)
+  (normalized : ∑ i, |state i|^2 = 1)
+
+-- Definition of a quantum verifier (Arthur)
+structure quantum_verifier :=
+  (verify : ℕ → bool) -- Simplified verification function
+
+-- Definition of a quantum prover (Merlin)
+structure quantum_prover :=
+  (prover : ℕ → quantum_state) -- Prover's strategy
+
+-- Definition of AWPP protocol
+structure awpp_protocol :=
+  (verifier : quantum_verifier)
+  (prover : quantum_prover)
+  (public_coins : list bool) -- Public coins accessible to both parties
+
+-- Generalized Probabilistic Theory
+structure bgp :=
+  (prob_space : Type) -- Type of probability spaces
+  (prob : prob_space → set ℕ) -- Probability distribution over natural numbers
+
+-- AWPP(BGP) complexity class
+def AWPP_BGP (P : awpp_protocol → bgp → Prop) :=
+  ∀ (ap : awpp_protocol) (bgp : bgp),
+    P ap bgp →
+    ∃ (poly : ℕ → ℕ), ∀ (n : ℕ),
+      let public_coins := list.repeat tt n in
+      let ap' := { ap with public_coins := public_coins } in
+      ap.verifier.verify n = tt →
+      let prob_dist := bgp.prob bgp.prob_space in
+      prob_dist {i | (ap'.prover.prover n).state i} ≥ 2/3 ∧ ap.verifier.verify n = ff →
+      prob_dist {i | (ap'.prover.prover n).state i} ≤ 1/3
+
+-- Example theorem: Quantum states are normalized
+theorem quantum_state_normalized (qs : quantum_state) :
+  ∑ i, |qs.state i|^2 = 1 :=
+qs.normalized
+
+end awpp
